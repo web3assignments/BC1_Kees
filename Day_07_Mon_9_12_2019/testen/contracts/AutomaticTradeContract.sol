1ce3a3f42fc2bb7724427285f9975e0c0480d2b5
KillahKeezy <57680944+KillahKeezy@users.noreply.github.com>
2019-12-11 08:58:07 +0100

    Using oracle in my smartcontract

--- Day_07_Mon_9_12_2019/testen/contracts/AutomaticTradeContract.sol
+++ Day_07_Mon_9_12_2019/testen/contracts/AutomaticTradeContract.sol
@@ -1,8 +1,26 @@
 pragma solidity ^0.5.12;
 
-contract AutomaticTradeContract {
-  
-  address public AddressContract;
+import "./provableAPI.sol";
+
+contract AutomaticTradeContract is usingProvable {
+   string  public alarm;
+   uint256 public priceOfUrl;
+   constructor() public payable {}
+
+   function __callback(bytes32 myid, string memory result ) public {
+       if (msg.sender != provable_cbAddress()) revert();
+       alarm = result;
+   }
+
+   function GetAlarm() public payable {
+       priceOfUrl = provable_getPrice("URL");
+       require (address(this).balance >= priceOfUrl,
+            "please add some ETH to cover for the query fee");
+       provable_query("URL", 
+            "json(http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam).liveweer[0].alarm");
+   }
+
+
   
   event Log(string message, uint amount, uint offerPotatos);
   
